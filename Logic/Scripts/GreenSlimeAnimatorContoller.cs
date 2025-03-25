2f06c36acae9fb46cf0f4eeda8e77be3510e1e47
Redream <mr.golyadkina@mail.ru>
2025-03-25 17:42:25 +0300

    Added vfx
    Added vfx when click on green slime

--- Logic/Scripts/GreenSlimeAnimatorContoller.cs
+++ Logic/Scripts/GreenSlimeAnimatorContoller.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class GreenSlimeAnimatorContoller : MonoBehaviour
 {
+    [SerializeField] private GameObject _clickVFX;
     private Animator _animator;
 
     private void Awake()
@@ -14,7 +15,7 @@ public class GreenSlimeAnimatorContoller : MonoBehaviour
     private void OnMouseUp()
     {
         _animator.SetTrigger("jump");
-
         AudioManager.Instance.PlaySFX("click_slime");
+        Instantiate(_clickVFX, gameObject.transform.position, Quaternion.identity);
     }
 }
