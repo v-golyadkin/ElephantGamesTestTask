c3c9183b7b7090d7716c10495f8a8aa42fa57da5
Redream <mr.golyadkina@mail.ru>
2025-03-26 12:20:04 +0300

    Added localization

---
+++ Logic/Scripts/LocaleSelector.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Localization.Settings;
+
+public class LocaleSelector : MonoBehaviour
+{
+    private bool _active = false;
+
+    public void ChangeLocale(int localeId)
+    {
+        if(_active == true)
+        {
+            return;
+        }
+
+        StartCoroutine(SetLocale(localeId));
+    }
+
+    private IEnumerator SetLocale(int localeId)
+    {
+        _active = true;
+
+        yield return LocalizationSettings.InitializationOperation;
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[localeId];
+
+        _active = false;
+    }
+}
